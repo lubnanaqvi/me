40823cf4949379a15bc8b4028911a1b4d20d95e7
Lubna Naqvi <lubnanaqvi11@gmail.com>
2019-05-26 14:08:11 -0400

    rose

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -14,7 +14,7 @@ const ProjectCard = ({ projectData, openedProject }) => {
     position: absolute;
     top: 20%;
     left: 0%;
-    background-color: #f8a39d;
+    background-color: #c2877b;
     color: black;
     border: solid 1px black;
     box-sizing: border-box;

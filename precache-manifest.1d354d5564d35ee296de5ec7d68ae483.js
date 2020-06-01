77dcacd1a45d0a7220718ba1ad4daa27ab85c6bf
Lubna Naqvi <lubnanaqvi11@gmail.com>
2020-06-01 00:59:24 -0400

    Updates

--- precache-manifest.1d354d5564d35ee296de5ec7d68ae483.js
+++ precache-manifest.1d354d5564d35ee296de5ec7d68ae483.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "41974f47e0a103c7be44e3270be969e1",
+    "revision": "bd156870132e6faa0846d55127086272",
     "url": "/me/index.html"
   },
   {
-    "revision": "7658e57324c62e93e183",
+    "revision": "6c30d046f3edfd9d8635",
     "url": "/me/static/css/main.829b11b5.chunk.css"
   },
   {
-    "revision": "031aeec8aeb941121a65",
-    "url": "/me/static/js/2.6056a394.chunk.js"
+    "revision": "66857358dc6834b7189b",
+    "url": "/me/static/js/2.be5a8642.chunk.js"
   },
   {
-    "revision": "7658e57324c62e93e183",
-    "url": "/me/static/js/main.df686f69.chunk.js"
+    "revision": "6c30d046f3edfd9d8635",
+    "url": "/me/static/js/main.abd04704.chunk.js"
   },
   {
     "revision": "66be97a19e235fa95a6c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.284500ad8dfa93e2544a4efb466ea0a3.js"
+  "/me/precache-manifest.1d354d5564d35ee296de5ec7d68ae483.js"
 );
 
 self.addEventListener('message', (event) => {
